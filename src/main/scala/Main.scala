5a0ac71ec4447a45fb30a64c33d31c8ff778e831
Yik San Chan <evan.chanyiksan@gmail.com>
2021-07-08 20:13:40 +0800

    use print connector in sink

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,35 @@
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.EnvironmentSettings
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+import org.apache.flink.api.scala._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+    val settings = EnvironmentSettings.newInstance.build
+    val execEnv = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv = StreamTableEnvironment.create(execEnv, settings)
+    tableEnv.executeSql(
+      """
+        |CREATE TABLE csv_source (
+        |  a int,
+        |  b int
+        |) WITH (
+        |  'connector' = 'filesystem',
+        |  'format' = 'csv',
+        |  'path' = '/tmp/input'
+        |)""".stripMargin)
+    tableEnv.executeSql(
+      """
+        |CREATE TABLE print_sink (
+        |  a int
+        |) WITH (
+        |  'connector' = 'print'
+        |)
+        |""".stripMargin)
+    tableEnv.executeSql(
+      """
+        |INSERT INTO print_sink
+        |SELECT a FROM csv_source
+        |""".stripMargin).await()
   }
 }
