3762a7022be3d6f45d7242493b6ead6f412d04c6
Yik San Chan <evan.chanyiksan@gmail.com>
2021-07-08 20:33:07 +0800

    print watermark=-9223372036854775808

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,8 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.table.api.EnvironmentSettings
 import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.flink.types.Row
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -27,7 +29,12 @@ object Main {
         |)
         |""".stripMargin)
     val table = tableEnv.sqlQuery("SELECT a FROM csv_source")
-    tableEnv.toDataStream(table).map(r => println(r.getField("a")))
+    tableEnv.toDataStream(table).addSink(new SinkFunction[Row] {
+      override def invoke(value: Row, context: SinkFunction.Context): Unit = {
+        println(s"watermark=${context.currentWatermark()}")
+        println(value.getField("a"))
+      }
+    })
     execEnv.execute()
   }
 }
